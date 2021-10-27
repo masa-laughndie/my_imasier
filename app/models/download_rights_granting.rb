5622fc78a144e8d130bff37dc311507b8559a930
masaya.takahashi <masaya.oor710@gmail.com>
2021-10-27 11:25:32 +0900

    remove duration value object

--- app/models/download_rights_granting.rb
+++ app/models/download_rights_granting.rb
@@ -5,21 +5,19 @@ class DownloadRightsGranting < ApplicationRecord
   has_many :plans, -> { distinct },
                    through: :download_rights_packings
 
-  composed_of :interval,
-              class_name: 'Duration',
-              mapping: [%w[interval_number number], %w[interval_unit unit]],
-              converter: :build
-
-  composed_of :valid_duration,
-              class_name: 'Duration',
-              mapping: [%w[valid_duration_number number], %w[valid_duration_unit unit]],
-              converter: :build
-
   validates :right_count,           presence: true, numericality: { only_integer: true }
   validates :interval_number,       presence: true, numericality: { only_integer: true }
   validates :interval_unit,         presence: true, inclusion: { in: TIME_UNITS }
   validates :valid_duration_number, presence: true, numericality: { only_integer: true }
   validates :valid_duration_unit,   presence: true, inclusion: { in: TIME_UNITS }
+
+  def interval
+    interval_number.__send__(interval_unit.to_sym)
+  end
+
+  def valid_duration
+    valid_duration_number.__send__(valid_duration_unit.to_sym)
+  end
 
   def max_carryover_number
     (valid_duration / interval) - 1
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -5,13 +5,12 @@ class Plan < ApplicationRecord
                                       dependent: :destroy
   has_many :download_rights_grantings, through: :download_rights_packings
 
-  composed_of :contract_duration,
-              class_name: 'Duration',
-              mapping: [%w[contract_duration_number number], %w[contract_duration_unit unit]],
-              converter: :build
-
   validates :price,                        presence: true
   validates :contract_duration_number,     presence: true, numericality: { only_integer: true }
   validates :contract_duration_unit,       presence: true, inclusion: { in: TIME_UNITS }
   validates :seats_count,                  presence: true, numericality: { only_integer: true }
+
+  def contract_duration
+    contract_duration_number.__send__(contract_duration_unit.to_sym)
+  end
 end
